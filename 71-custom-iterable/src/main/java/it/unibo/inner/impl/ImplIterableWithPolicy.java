7a1e2a1d178422b26188078ff2a0b0b7fc1bf9d0
Chiara Mazzoni <chiara.mazzoni12@studio.unibo.it>
2025-11-04 16:05:30 +0100

    New folder created

---
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/ImplIterableWithPolicy.java
@@ -0,0 +1,10 @@
+package main.java.it.unibo.inner.impl;
+
+import it.unibo.inner.api.IterableWithPolicy;
+
+public class ImplIterableWithPolicy<T> implements IterableWithPolicy<T>{
+    private T[] elements;
+    public ImplIterableWithPolicy(T[] elements) {
+
+    }
+}
