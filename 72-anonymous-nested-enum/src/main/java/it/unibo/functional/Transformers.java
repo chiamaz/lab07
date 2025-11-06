3af8e379ce7a847d88aebab16fad795e6385feae
Chiara Mazzoni <chiara.mazzoni12@studio.unibo.it>
2025-11-06 14:39:12 +0100

    Exerxise 72, part 1 and 2 finished

--- 72-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
@@ -54,9 +54,12 @@ public final class Transformers {
      * @return A transformed list where each input element is replaced with the produced elements
      */
     public static <I, O> List<O> transform(final Iterable<I> base, final Function<I, O> transformer) {
-        return null;
+        final var result = new ArrayList<O>();
+        for (final I input : Objects.requireNonNull(base, "The base iterable cannot be null")) {
+            result.add(transformer.call(input));
+        }
+        return result.;
     }
-
     /**
      * A function that takes an iterable of collections, and returns a flatten list of the elements of the inner
      * collections.
